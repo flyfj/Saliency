8e0bbaeadaca99225f23a94c98ba3e15dee0dc2f
flyfj <fj333xx@gmail.com>
2014-03-12 16:23:47 -0400

    enable to display 3d image

--- SmartWindows/Matlab/visualize_rgbd.m
+++ SmartWindows/Matlab/visualize_rgbd.m
@@ -8,41 +8,41 @@ usedepth = 1;
 imgfile = 'E:\Datasets\RGBD Dataset\NYU\img\115_img.png';
 depthfile = 'E:\Datasets\RGBD Dataset\NYU\img\115_depth.txt';
 
+scale = 0.4;
+
 img = imread(imgfile);
-mat3d(img)
-pause
+img = imresize(img, scale);
+% mat3d(img)
+% pause
 [m ,n,c] = size(img);
-% m = 100, n = 100;
 X = repmat(1:n, m, 1);
 Y = repmat((1:m)', 1, n);
 
 % convert to gray
 grayimg = rgb2gray(img);
-grayimg = double(grayimg);
-grayimg = imresize(grayimg, 0.2);
+% grayimg = double(grayimg);
 
 dmap = load(depthfile);
+dmap = 1./ dmap;
+dmap = imresize(dmap, scale);
 
 % visualize in a row
 figure(1)
-% [xx, yy] = meshgrid(1: size(grayimg, 1), 1: size(grayimg, 2));
-% scatter3(xx(:), yy(:), grayimg(:), grayimg(:));
-surf(grayimg, 'EdgeColor','none')
-colormap jet
-hold on
-
-subplot(1,3,1)
+subplot(2,1,1)
 imshow(img)
-hold on
+subplot(2,1,2)
+imshow(grayimg)
 
-subplot(1,3,2)
-surf(X, Y, grayimg)
-hold on
+figure(2)
+surf(grayimg, 'FaceColor', 'texturemap')
+colormap jet
+colorbar
 
 if usedepth
-    subplot(1,3,3)
-    surf(X, Y, dmap)
-    hold on
+    figure(3)
+    surf(dmap, 'FaceColor', 'texturemap')
+    colormap jet
+    colorbar
 end
 
 pause
