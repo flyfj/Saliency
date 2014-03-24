64598d3f0fcee3fa56cfecaf33d995c56390390f
flyfj <fj333xx@gmail.com>
2014-03-24 18:03:06 -0400

    added analyzer and some example images

---
+++ SmartWindows/Matlab/objectAnalyzer.m
@@ -0,0 +1,57 @@
+
+imgfile = 'E:\Datasets\RGBD Dataset\NYU\img\115_img.png';
+depthfile = 'E:\Datasets\RGBD Dataset\NYU\img\115_depth.txt';
+
+
+%% analyze boundary in images
+
+cimg = imread('red.jpg');
+
+visualize_rgbd(cimg);
+
+grayimg = rgb2gray(cimg);
+figure(1)
+title('input image')
+hold on
+subplot(1, 2, 1)
+imshow(cimg)
+subplot(1, 2, 2)
+imshow(grayimg)
+
+% compute edge
+% rimg = cimg(:,:,1);
+% gimg = cimg(:,:,2);
+% bimg = cimg(:,:,3);
+% [rgmag, rgdir] = imgradient(rimg, 'sobel');
+% [ggmag, ggdir] = imgradient(gimg, 'sobel');
+% [bgmag, bgdir] = imgradient(bimg, 'sobel');
+% gmag = sqrt(rgmag.^2 + ggmag.^2 + bgmag.^2);
+
+[gx, gy] = imgradientxy(grayimg, 'prewitt');
+[gmag, gdir] = imgradient(grayimg, 'prewitt');
+
+figure(2)
+title('Gradient magnitude')
+hold on
+imshow(gmag, [])
+
+% visualize_rgbd(gmag)
+
+figure(3)
+title('Gradient direction')
+hold on
+subplot(1, 3, 1)
+imshow(gdir, [])
+subplot(1, 3, 2)
+imshow(gx, [])
+subplot(1, 3, 3)
+imshow(gy, [])
+
+
+figure(4)
+[X, Y] = meshgrid(1:2:size(grayimg, 2), 1:2:size(grayimg, 1));
+quiver(X(:), Y(:), gx(sub2ind(size(gx), Y(:), X(:))), gy(sub2ind(size(gy), Y(:), X(:))))
+
+
+pause
+close all
--- SmartWindows/Matlab/visualize_rgbd.m
+++ SmartWindows/Matlab/visualize_rgbd.m
@@ -1,35 +1,31 @@
+function [] = visualize_rgbd(cimg, dmap)
+% input: color image and depth map
+
 
 %% to visualize rgbd images as 3d
 
-clear
-
-usedepth = 1;
-
-imgfile = 'E:\Datasets\RGBD Dataset\NYU\img\115_img.png';
-depthfile = 'E:\Datasets\RGBD Dataset\NYU\img\115_depth.txt';
+if ~exist('dmap', 'var')
+    usedepth = 0;
+else
+    usedepth = 1;
+end
 
 scale = 0.4;
 
-img = imread(imgfile);
-img = imresize(img, scale);
-% mat3d(img)
-% pause
-[m ,n,c] = size(img);
-X = repmat(1:n, m, 1);
-Y = repmat((1:m)', 1, n);
+cimg = imresize(cimg, scale);
 
-% convert to gray
-grayimg = rgb2gray(img);
-% grayimg = double(grayimg);
-
-dmap = load(depthfile);
-dmap = 1./ dmap;
-dmap = imresize(dmap, scale);
+if length(size(cimg)) == 2
+    grayimg = cimg;
+else
+    % convert to gray
+    grayimg = rgb2gray(cimg);
+    % grayimg = double(grayimg);
+end
 
 % visualize in a row
 figure(1)
 subplot(2,1,1)
-imshow(img)
+imshow(cimg)
 subplot(2,1,2)
 imshow(grayimg)
 
@@ -39,6 +35,10 @@ colormap jet
 colorbar
 
 if usedepth
+  
+    dmap = 1./ dmap;
+    dmap = imresize(dmap, scale);
+    
     figure(3)
     surf(dmap, 'FaceColor', 'texturemap')
     colormap jet
@@ -47,3 +47,5 @@ end
 
 pause
 close all
+
+end
