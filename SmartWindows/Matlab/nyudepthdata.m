08f1edbd3ac8abb6a7dc1e640edc82165b197fc7
flyfj <fj333xx@gmail.com>
2014-05-25 15:49:33 -0400

    added nyudataparser

---
+++ SmartWindows/Matlab/nyudepthdata.m
@@ -0,0 +1,13 @@
+
+savepath = 'E:\Datasets\RGBD_Dataset\NYU\Depth2\';
+
+% parse nyu data
+nyudepth = load('E:\Datasets\RGBD_Dataset\NYU\nyu_depth_v2_labeled.mat');
+
+allcimgs = nyudepth.images;
+alldimgs = nyudepth.depths;
+alllabels = nyudepth.labels;
+
+[imgh, imgw, imgnum] = size(allcimgs);
+
+
