5c591428b904a521fd1b664c8519d9b39bd2add0
flyfj <fj333xx@gmail.com>
2014-06-06 18:23:39 -0400

    added code to parse nyudepth to txt

--- SmartWindows/Matlab/parsenyudatatofiles.m
+++ SmartWindows/Matlab/parsenyudatatofiles.m
@@ -5,11 +5,11 @@
 savepath = 'E:\Datasets\RGBD_Dataset\NYU\Depth2\';
 
 % parse nyu data
-nyudata = load('E:\Datasets\RGBD_Dataset\NYU\nyu_depth_v2_labeled.mat');
-nyuimgs = nyudata.images;
-nyudepth = nyudata.depths;
-nyulabels = nyudata.labels;
-clearvars nyudata
+% nyudata = load('E:\Datasets\RGBD_Dataset\NYU\nyu_depth_v2_labeled.mat');
+% nyuimgs = nyudata.images;
+% nyudepth = nyudata.depths;
+% nyulabels = nyudata.labels;
+% clearvars nyudata
 
 % get boundary points for each image
 [imgh, imgw, imgnum] = size(nyudepth);
@@ -17,10 +17,15 @@ clearvars nyudata
 % output images
 for i=1:imgnum
     imgfn = [savepath num2str(i) '.jpg'];
-    imwrite(nyuimgs(:,:,:,i), imgfn, 'JPEG'); 
+    imwrite(nyuimgs(:,:,:,i), imgfn, 'JPEG');
+    depthfn = [savepath num2str(i) '_d.txt'];
     imgfn = [savepath num2str(i) '_d.mat'];
     depth = nyudepth(:,:,i);
-    save(imgfn, 'depth');
+    fid = fopen(depthfn, 'w');
+    fprintf(fid, '%d %d\n', imgw, imgh);
+    fprintf(fid, '%d ', depth');
+    fclose(fid);
+    %save(imgfn, 'depth');
     imgfn = [savepath num2str(i) '_l.png'];
     imwrite(nyulabels(:,:,i), imgfn, 'PNG', 'BitDepth', 16);
     
